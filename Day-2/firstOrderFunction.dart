ddb7ff695593b80d028a79e85befb997588cbdf6
Injamul Haq Sohag <99319134+Sohag-84@users.noreply.github.com>
2022-06-08 12:42:19 +0600

    ..

---
+++ Day-2/firstOrderFunction.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter your first number: ");
+  int firstNum = int.parse(stdin.readLineSync() ?? '');
+  stdout.write("Enter your second number: ");
+  int secondNum = int.parse(stdin.readLineSync() ?? '');
+
+  var addResult = calculator(firstNum, secondNum, addition);
+  print("Addition result: $addResult");
+
+  //for multiply result:
+  var multiplyResult = calculator(firstNum, secondNum, multiply);
+  print("Addition result: $multiplyResult");
+}
+
+int calculator(int firstNum, int secondNum, Function calculation) {
+  return calculation(firstNum, secondNum);
+}
+
+int addition(int firstNum, int secondNum) {
+  return (firstNum + secondNum);
+}
+
+int multiply(int firstNum, int secondNum) {
+  return (firstNum * secondNum);
+}
+
+int substructin(int firstNum, int secondNum) {
+  return (firstNum - secondNum);
+}
+
+double division(int firstNum, int secondNum) {
+  return (firstNum / secondNum);
+}
